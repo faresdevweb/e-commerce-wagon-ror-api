11aa830ab255a6913f4b80e5fcf8fa4a502e8742
faresdevweb <fares.meier@hotmail.com>
2024-06-14 12:02:57 +0200

    setup routes for signup and signin

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  post 'auth/login', to: 'authentication#login'
+  post 'auth/signup', to: 'authentication#signup'
 end
