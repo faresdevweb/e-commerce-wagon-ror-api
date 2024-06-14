0246e40c8c50ea3cf3607be2172c77825cd9570b
faresdevweb <fares.meier@hotmail.com>
2024-06-14 12:16:23 +0200

    run previous migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_095304) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_14_101527) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
