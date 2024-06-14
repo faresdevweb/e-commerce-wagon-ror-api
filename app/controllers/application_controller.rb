b6ddd6baa40688576b632fd1a5b8dc9cf801fe69
faresdevweb <fares.meier@hotmail.com>
2024-06-14 12:02:40 +0200

    setup jwt module and auth middleware in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        JsonWebToken.encode(payload)
+      end
+    
+      def authorize_request
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        decoded = JsonWebToken.decode(header)
+        @current_user = User.find(decoded[:user_id]) if decoded
+      rescue
+        render json: { errors: 'Unauthorized' }, status: :unauthorized
+      end
 end
