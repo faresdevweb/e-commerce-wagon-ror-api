617ece55883ea890c51d25dbda07919eea0ae3b4
faresdevweb <fares.meier@hotmail.com>
2024-06-21 10:41:40 +0200

    run migration for carts and carts items

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_102300) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_21_083417) do
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id", null: false
+    t.integer "article_id", null: false
+    t.integer "quantity", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_cart_items_on_article_id"
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -34,4 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_102300) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cart_items", "articles"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "carts", "users"
 end
