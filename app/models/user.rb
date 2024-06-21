8019b2b96c6730df262c7070e0721a369952aa31
faresdevweb <fares.meier@hotmail.com>
2024-06-21 10:51:15 +0200

    updated model user with dependent destroy on carts model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_one :cart, dependent: :destroy
 
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { minimum: 6 }
