e374facbc43a50e6e07c17d372db16958e1c31b7
faresdevweb <fares.meier@hotmail.com>
2024-06-21 11:31:16 +0200

    updated model user with has many dependent destroy on reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     has_one :cart, dependent: :destroy
-
+    has_many :reviews, dependent: :destroy
+    
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { minimum: 6 }
 end
